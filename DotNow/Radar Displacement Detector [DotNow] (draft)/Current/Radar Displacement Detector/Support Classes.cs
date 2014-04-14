b9cdb608079201bef1e1514f384737a5b07d5bce
BlearyEye <william.leal@samraksh.info>
2014-04-14 16:24:19 -0400

    Displacement Detector ... add in running average

--- DotNow/Radar Displacement Detector [DotNow] (draft)/Current/Radar Displacement Detector/Support Classes.cs
+++ DotNow/Radar Displacement Detector [DotNow] (draft)/Current/Radar Displacement Detector/Support Classes.cs
@@ -61,6 +61,47 @@ namespace Samraksh.AppNote.DotNow.RadarDisplacementDetector {
     }
 
     /// <summary>
+    /// Keep a running total
+    /// </summary>
+    public static class SampleAverage {
+        private const int Bits = 8;
+        private static readonly Sample[] SampBuffer = new Sample[2 ^ Bits];
+        private static int _sampBufferPtr = 0;
+        private static Sample _sum = new Sample();
+        private static Sample _retVal = new Sample();
+
+        /// <summary>
+        /// Return the average
+        /// </summary>
+        public static Sample Average {
+            get {
+                _retVal.I = _sum.I << Bits;
+                _retVal.Q = _sum.Q << Bits;
+                return _retVal;
+            }
+        }
+
+        /// <summary>
+        /// Add in a new sample
+        /// </summary>
+        /// <param name="theSample">Sample to add in</param>
+        public static void AddSample(Sample theSample) {
+            // Subtract the values of the sample that's going away
+            _sum.I -= SampBuffer[_sampBufferPtr].I;
+            _sum.Q -= SampBuffer[_sampBufferPtr].Q;
+            // Add the new sample value
+            _sum.I += theSample.I;
+            _sum.Q += theSample.Q;
+            // Move in the new sample
+            SampBuffer[_sampBufferPtr] = theSample;
+            // Update the pointer
+            _sampBufferPtr = (_sampBufferPtr + 1) % SampBuffer.Length;
+        }
+        
+    }
+
+
+    /// <summary>
     /// Define GPIO ports
     /// </summary>
     public static class GpioPorts {
