8631405649a2b64f4ddf5f82321c6996f0353de0
BlearyEye <william.leal@samraksh.info>
2014-04-25 15:29:49 -0400

    Data Collector

--- DotNow/Data Collection Exfiltration [DotNow] (draft)/Current/Data Collection/Data Collection/Program.cs
+++ DotNow/Data Collection Exfiltration [DotNow] (draft)/Current/Data Collection/Data Collection/Program.cs
@@ -27,7 +27,7 @@ namespace Samraksh.AppNote.DotNow.DataCollector {
         public const int SamplingIntervalMilliSec = 4000;    // Larger values => fewer samples/sec
         /// <summary>Number of samples to collect before presenting for processing</summary>
         //public const int BufferSize = 500;
-        public const int BufferSize = 500;
+        public const int BufferSize = 512;
         /// <summary>Number of samples per second</summary>
         public const int SamplesPerSecond = 1000000 / SamplingIntervalMilliSec;
         /// <summary>Number of microseconds between invocation of buffer processing callback</summary>
@@ -57,12 +57,16 @@ namespace Samraksh.AppNote.DotNow.DataCollector {
         private static readonly ushort[] Ibuffer = new ushort[AdcParameters.BufferSize];
         private static readonly ushort[] Qbuffer = new ushort[AdcParameters.BufferSize];
 
+        private static readonly ushort[] IQbuffer = new ushort[AdcParameters.BufferSize * 2];
+
         private static readonly EmoteLcdUtil Lcd = new EmoteLcdUtil();
 
-        private static readonly DataStore Dstore =  DataStore.Instance(STORAGE_TYPE.NOR);
+        private static readonly DataStore Dstore = DataStore.Instance(STORAGE_TYPE.NOR);
         private static DATASTORE_RETURN_STATUS _retVal;
 
-        private static readonly AutoResetEvent WaitForCollectionDone = new AutoResetEvent(false);
+        private static readonly AutoResetEvent StartSavingCollectionData = new AutoResetEvent(false);
+
+        private static int _dataReferencesIn, _dataItemsIn;
 
         /// <summary>
         /// Get things started
@@ -73,7 +77,7 @@ namespace Samraksh.AppNote.DotNow.DataCollector {
 
             VersionInfo.Init(Assembly.GetExecutingAssembly());
             Debug.Print("Data Collector " + VersionInfo.Version + " (" + VersionInfo.BuildDateTime + ")");
-            Lcd.Display("d c");
+            Lcd.Display("d c2");
 
             Debug.Print("Power Level: " + PowerState.CurrentPowerLevel + " (16=High, 32=Med, 48=Low");
             PowerState.ChangePowerLevel(PowerLevel.High);
@@ -107,13 +111,11 @@ namespace Samraksh.AppNote.DotNow.DataCollector {
             Debug.Print("SD initialized");
 
             // Store the sampling info first
-            var parameters = new[] { (uint)AdcParameters.SamplesPerSecond };
-            var dref = new DataReference(Dstore, parameters.Length, REFERENCE_DATA_TYPE.UINT32);
-            if ((_retVal = dref.Write(parameters, 0, parameters.Length)) != DATASTORE_RETURN_STATUS.Success) {
-                throw new Exception("Failed write data reference for samples per second" + _retVal);
-            }
-            Debug.Print("Stored the samples per second");
+            var samplesPerSecondBytes = new byte[4];
+            ConvertUintToByteArray(AdcParameters.SamplesPerSecond, samplesPerSecondBytes);
+            SD.Write(samplesPerSecondBytes, 0, 4);
 
+            Debug.Print("Stored the samples per second value");
 
             // Start collecting sample data
             var processSampleBufferThread = new Thread(ProcessSampleBuffer);
@@ -126,16 +128,61 @@ namespace Samraksh.AppNote.DotNow.DataCollector {
             //  Doing it this way since Data Store might fill up first; it can also signal that collection is done
             (new Thread(() => {
                 Thread.Sleep(AdcParameters.SamplingDurationSec * 1000);
-                WaitForCollectionDone.Set();
+                Debug.Print("Collected for " + AdcParameters.SamplingDurationSec + " seconds");
+                StartSavingCollectionData.Set();
             })).Start();
 
+            // Wait till the collection time has elapsed or the Data Store is filled up
+            StartSavingCollectionData.WaitOne();
 
-            WaitForCollectionDone.WaitOne();
-
-            // Stop sampling and terminate
+            // Stop collecting
             AnalogInput.StopSampling();
             Debug.Print("* Collection stopped");
 
+            // Copy from Data Store to SD
+            var dataReferences = new DataReference[10];
+            var offset = 0;
+            var dataReferencesOut = 0;
+            var dataItemsOut = 0;
+            var sampleBytes = new byte[2];
+            while (true) {
+                if ((_retVal = Dstore.ReadAllDataReferences(dataReferences, offset)) != DATASTORE_RETURN_STATUS.Success) {
+                    throw new Exception("Cannot get data references from offset " + offset + "; return value " + _retVal);
+                }
+                offset += dataReferences.Length;
+                dataReferencesOut++;
+                foreach (var theReference in dataReferences) {
+                    dataItemsOut++;
+                    if (dataItemsOut % 10 == 0) {
+                        Debug.Print("  Writing sample " + dataItemsOut);
+                    }
+                    if (theReference == null) {
+                        goto finished;
+                    }
+                    if (theReference.getDataReferenceSize != IQbuffer.Length) {
+                        throw new Exception("Data reference size " + theReference.getDataReferenceSize + " is not equal to IQbuffer length " + IQbuffer.Length);
+                    }
+                    if (theReference.getDataReferenceType != typeof(ushort)) {
+                        throw new Exception("Data reference type is " + theReference.getDataReferenceType + "; must be ushort");
+                    }
+                    if ((_retVal = theReference.Read(IQbuffer, 0, IQbuffer.Length)) != DATASTORE_RETURN_STATUS.Success) {
+                        throw new Exception("Cannot read data from allocation (DS); return value " + _retVal);
+                    }
+                    foreach (var theSample in IQbuffer) {
+                        ConvertUshortToByteArray(theSample, sampleBytes);
+                    }
+                    SD.Write(sampleBytes, 0, (ushort)sampleBytes.Length);
+                }
+            }
+        finished:
+            
+            Debug.Print("\nFinished writing to SD");
+
+            Debug.Print("");
+            var dr = "Data References " + _dataReferencesIn + " in, " + dataReferencesOut + " out";
+            Debug.Print(_dataReferencesIn == dataReferencesOut ? "" : "***** " + dr);
+            var ditems = "Data Items" + _dataItemsIn + " in, " + dataItemsOut + " out";
+            Debug.Print(_dataItemsIn == dataItemsOut ? "" : "***** " + ditems);
         }
 
         private static int _currentlyProcessingBuffer = IntBool.False;
@@ -159,22 +206,35 @@ namespace Samraksh.AppNote.DotNow.DataCollector {
             ProcessSampleBufferEvent.Set();
         }
 
-        private static readonly byte[] DataArray = new byte[4];
-
         private static void ProcessSampleBuffer() {
             while (true) {
                 ProcessSampleBufferEvent.WaitOne();
                 var startProcessing = DateTime.Now;
                 for (var i = 0; i < Ibuffer.Length; i++) {
-                    DataArray[0] = (byte)(Ibuffer[i] >> 8);
-                    DataArray[1] = (byte)(Ibuffer[i] & 0xff);
-                    DataArray[2] = (byte)(Qbuffer[i] >> 8);
-                    DataArray[3] = (byte)(Qbuffer[i] & 0xff);
+                    var iqIndex = i << 1;
+                    IQbuffer[iqIndex] = Ibuffer[i];
+                    IQbuffer[iqIndex | 1] = Qbuffer[i];
                     if (i % 10 == 0) {
                         Debug.Print("I: " + Ibuffer[i] + " Q: " + Qbuffer[i]);
                     }
-                    SD.Write(DataArray, 0, (ushort)DataArray.Length);
                 }
+
+                var free = Dstore.FreeBytes;
+                if (free < IQbuffer.Length * 2) {
+                    Debug.Print("Filled up data store");
+                    StartSavingCollectionData.Set();
+                    return;
+                }
+
+                var dref = new DataReference(Dstore, IQbuffer.Length, REFERENCE_DATA_TYPE.UINT32);
+                if ((_retVal = dref.Write(IQbuffer, 0, IQbuffer.Length)) != DATASTORE_RETURN_STATUS.Success) {
+                    throw new Exception("Failed write data reference" + _retVal);
+                }
+
+                // Counters for later verification
+                _dataReferencesIn++;
+                _dataItemsIn += IQbuffer.Length;
+
                 var processingTime = DateTime.Now - startProcessing;
                 Debug.Print("\nProcessing Time: " + (processingTime.Seconds * 1000 + processingTime.Milliseconds) + ", Callback Interval: " + AdcParameters.CallbackIntervalMs);
                 Debug.Print("");
@@ -185,6 +245,29 @@ namespace Samraksh.AppNote.DotNow.DataCollector {
         private struct IntBool {
             public const int True = 1;
             public const int False = 0;
+        }
+
+        /// <summary>
+        /// Convert Uint to Byte Array
+        /// </summary>
+        /// <param name="input">Input value</param>
+        /// <param name="output">Byte array; must be size 4 or larger, else exception</param>
+        private static void ConvertUintToByteArray(uint input, byte[] output) {
+            output[0] = (byte)(input >> 24);
+            output[1] = (byte)((input >> 16) & 0xff);
+            output[2] = (byte)((input >> 8) & 0xff);
+            output[3] = (byte)(input & 0xff);
+        }
+
+        /// <summary>
+        /// Convert Ushort to Byte Array
+        /// </summary>
+        /// <param name="input">Input value</param>
+        /// <param name="output">Byte array; must be size 2 or larger, else exception</param>
+        private static void ConvertUshortToByteArray(ushort input, byte[] output) {
+            output[0] = (byte)(input >> 8);
+            output[1] = (byte)(input & 0xff);
+
         }
 
     }
