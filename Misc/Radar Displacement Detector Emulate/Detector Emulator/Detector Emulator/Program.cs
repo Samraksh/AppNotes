4892d3d0c58eb2e739f7554604a2606eef1db20b
William Leal <william.leal@samraksh.info>
2014-06-02 10:31:12 -0400

    Displacement Detector validation / simulation

---
+++ Misc/Radar Displacement Detector Emulate/Detector Emulator/Detector Emulator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.SPOT;
+//using Samraksh.SPOT.Emulator;
+using System.Threading;
+using Samraksh.SPOT.Emulator;
+
+namespace Detector_Emulator {
+    public class Program {
+        public static void Main() {
+            int x = 3;
+            var adcConfig = new ADC_Configuration();
+            var adc = new ADC(adcConfig);
+            ADC.Init(4000);
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+    }
+}
