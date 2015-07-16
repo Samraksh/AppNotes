6a3ec289217071e7e48565dac9644eef20a4fb3c
William Leal <william.leal@samraksh.info>
2015-07-16 14:54:10 -0400

    arduing BB - validation

--- Arduino/Apps/Bumblebee_Displacement_Detector/Validation/ValidateArduinoDetects.m
+++ Arduino/Apps/Bumblebee_Displacement_Detector/Validation/ValidateArduinoDetects.m
@@ -3,163 +3,96 @@ if ~exist('ArduinoInputsDetects','var')
     [~,~,ArduinoInputsDetects] = xlsread('Arduino Log - InputsDetects.xlsx');
 end
 
-LineTypeC = 2;
-% SampleNoC = 3;
-% IValC = 4;
-% QValC = 5;
-% SumIC = 6;
-% SumQC = 7;
-% SampIC = 8;
-% SampQC = 9;
-% AdjIC = 10;
-% AdjQC = 11;
-% PrevAdjIC = 12;
-% PrevAdjQC = 13;
-
 SnippetSize = 250;
 MinCumCuts = 6;
 MofN_M = 2;
 MofN_N = 8;
 MofNBuff = zeros(1,MofN_N);
 
+[NRows, NCols] = size(ArduinoInputsDetects);
+TagCol = -1;
+for I = 1:NCols
+    if strcmp(ArduinoInputsDetects(1,I),'#c')
+        TagCol = I;
+        break;
+    end
+end
 
-SampleNoC = 3;
-AdjIC = 4;
-AdjQC = 5;
-IsCutC = 6;
-%CurrCutsC = 15;
-%RunCutsC = 16;
-DispC = 7;
-ConfC = 8;
+if TagCol < 1
+    fprintf('Tag column not found\nTerminating\n');
+    return;
+end
 
-% SumIValsM = 0;
-% SumQValsM = 0;
+LineTypeC = TagCol + 0;
+SampleNoC = TagCol + 1;
+AdjIC = TagCol + 2;
+AdjQC = TagCol + 3;
+IsCutC = TagCol + 4;
+IsDispC = TagCol + 5;
+IsConfC = TagCol + 6;
 
-% SamplingErrors = 0;
-% InterpolationErrors = 0;
-% MeanErrors = 0;
+InputCount = 0;
+SnippetCount = 0;
+CrossAngleErrors = 0;
 CutErrors = 0;
-%CurrCutsErrors = 0;
-%RunCutsErrors = 0;
 DispErrors = 0;
 ConfErrors = 0;
 
 CurrCutsM = 0;
-%RunCutsM = 0;
 
-for Idx = 1 : length(ArduinoInputsDetects)-1
-    if ~strcmp(ArduinoInputsDetects(Idx, LineTypeC), '#5') || isnan(ArduinoInputsDetects{Idx+1,1})
+for Idx = 1 : NRows - 1
+    NextCol1 = ArduinoInputsDetects{Idx+1,1};
+    if ~strcmp(ArduinoInputsDetects(Idx, LineTypeC), '#j') || isnan(NextCol1(1))
         continue;
     end
     
     SampleNo = ArduinoInputsDetects{Idx, SampleNoC};
-
-%     if SampleNo == 4251
-%     end
-%     
-
+    if SampleNo < 2
+        continue;
+    end;
     
-    %IA = ArduinoInputsDetects{Idx, IValC};
-    %QA = ArduinoInputsDetects{Idx, QValC};
+    InputCount = InputCount + 1;
     
-%     if mod(SampleNo,SnippetSize) == 0
-%         fprintf('* Snippet %i, Sample %i\n',mod(SampleNo,SnippetSize), SampleNo);
-%     end
-    
-%     SumIValsM = SumIValsM + IA;
-%     SumQValsM = SumQValsM + QA;
-    
-     if SampleNo < 2
-         continue;
-     end;
-%     
-%     SumIA = ArduinoInputsDetects{Idx, SumIC};
-%     SumQA = ArduinoInputsDetects{Idx, SumQC};
-%     PrevSampIA = ArduinoInputsDetects{Idx-1, SampIC};
-%     PrevSampQA = ArduinoInputsDetects{Idx-1, SampQC};
-     AdjIA = ArduinoInputsDetects{Idx, AdjIC};
-     AdjQA = ArduinoInputsDetects{Idx, AdjQC};
-     PrevAdjIA = ArduinoInputsDetects{Idx-1, AdjIC};
-     PrevAdjQA = ArduinoInputsDetects{Idx-1, AdjQC};
+    CurrIA = ArduinoInputsDetects{Idx, AdjIC};
+    CurrQA = ArduinoInputsDetects{Idx, AdjQC};
+    PrevIA = ArduinoInputsDetects{Idx-1, AdjIC};
+    PrevQA = ArduinoInputsDetects{Idx-1, AdjQC};
     
     IsCutA = ArduinoInputsDetects{Idx, IsCutC};
-    %CurrCutsA = ArduinoInputsDetects{Idx, CurrCutsC};
-    %RunCutsA = ArduinoInputsDetects{Idx, RunCutsC};
-    %IsDispA = ArduinoInputsDetects{Idx, DispC};
-    %IsConfA = ArduinoInputsDetects{Idx, ConfC};
     
-    %PrevAdjIM = ArduinoInputsDetects{Idx-1, AdjIC};
-    %PrevAdjQM = ArduinoInputsDetects{Idx-1, AdjQC};
+    PrevAngle = atan2(PrevIA, PrevQA);
+    CurrAngle = atan2(CurrIA, CurrQA);
     
-    
-%     % Check integrity of the data
-%     
-%     if (PrevSampIA < 0 && PrevSampQA < 0) || (PrevSampIA >= 0 && PrevSampQA >= 0)
-%         fprintf('Sampling Error. SampleNo %i\n', SampleNo);
-%         SamplingErrors = SamplingErrors + 1;
-%     end
-%     
-%     if PrevSampIA >= 0
-%         QValM = floor((ArduinoInputsDetects{Idx-1,QValC} + ArduinoInputsDetects{Idx+1,QValC})/2);
-%         if (IA ~= PrevSampIA) || (QA ~= QValM)
-%             fprintf('Interpolation Error. SampleNo %i\n', SampleNo);
-%             InterpolationErrors = InterpolationErrors + 1;
-%         end
-%     end
-%     
-%     if PrevSampQA >= 0
-%         IValM = floor((ArduinoInputsDetects{Idx-1,IValC} + ArduinoInputsDetects{Idx+1,IValC})/2);
-%         if (QA ~= PrevSampQA) || (IA ~= IValM)
-%             fprintf('Interpolation Error. SampleNo %i\n', SampleNo);
-%             InterpolationErrors = InterpolationErrors + 1;
-%         end
-%     end
-%     
-%     MeanIValsM = floor(SumIValsM / SampleNo);
-%     MeanQValsM = floor(SumQValsM / SampleNo);
-%     
-%     AdjustedIM = IA - MeanIValsM;
-%     AdjustedQM = QA - MeanQValsM;
-%     
-%     if (AdjustedIM ~= AdjIA) || (AdjustedQM ~= AdjQA)
-%         fprintf('Mean Calculation Error. SampleNo %i\n', SampleNo);
-%         MeanErrors = MeanErrors + 1;
-%     end
-    
-    % Check the cut analysis using previous and current values
-    %   We've validated the mean-adjusted sample inputs so we'll just use
-    %   them.
-            
-    PrevAngle = atan2(PrevAdjIA, PrevAdjQA);
-    %     if PrevAngle < 0
-    %         PrevAngle = PrevAngle + 2*pi;
-    %     end
-    Angle = atan2(AdjIA, AdjQA);
-    %     if Angle < 0
-    %         Angle = Angle + 2*pi;
-    %     end
-    %     Diff1 = mod(abs(Angle - PrevAngle), 2*pi);
-    %     if Diff1 > pi
-    %         Diff = -(2*pi - Diff1);
-    %     else
-    %         Diff = Diff1;
-    %     end
-    %Diff = Angle - PrevAngle;
-    
-    Diff = angleDiff(PrevAngle, Angle);
-    if round(mod(Diff,pi),5) == 0
-        Diff = 0;
+    DiffAngle = angleDiff(PrevAngle, CurrAngle);
+    % Ignore the case that one of the points is (0,0)
+    if (CurrIA == 0 && CurrQA == 0) || (PrevIA == 0 && PrevQA == 0)
+        DiffAngle = 0;
     end
+    % Treat angles close to pi as zero
+    if round(mod(DiffAngle,pi),5) == 0
+        DiffAngle = 0;
+    end
+    
+    % Check that the cross product (used by the mote program) matches the
+    % angle difference used here for verification
+    
+    CrossProduct = (PrevQA * CurrIA) - (PrevIA * CurrQA);
+    
+    if sign(CrossProduct) ~= sign(DiffAngle)
+        CrossAngleErrors = CrossAngleErrors + 1;
+    end
+    
+    % Validate cut
     
     IsCutM = 0;
     
-    % Clockwise: See if going from negative to positive
-    if Diff < 0 && PrevAdjIA < 0 && AdjIA > 0
+    % Counter clockwise: See if going from negative to positive
+    if DiffAngle < 0 && PrevQA > 0 && CurrQA < 0
         IsCutM = +1;
     end
     
-    % Counter-clockwise: See if going from positive to negative
-    if Diff > 0 && PrevAdjIA > 0 && AdjIA < 0
+    % Clockwise: See if going from positive to negative
+    if DiffAngle > 0 && PrevQA < 0 && CurrQA > 0
         IsCutM = -1;
     end
     
@@ -167,37 +100,37 @@ for Idx = 1 : length(ArduinoInputsDetects)-1
         fprintf('Cut Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, IsCutA, IsCutM);
         CutErrors = CutErrors + 1;
         
-        Cross = (PrevAdjQA * AdjIA) - (PrevAdjIM * AdjQA);
+        Cross = (PrevQA * CurrIA) - (PrevIA * CurrQA);
         fprintf('SampNo: %i\n', SampleNo);
-        fprintf('Prev (I,Q) Ard: (%i,%i)\n',PrevAdjIA, PrevAdjQA);
-        fprintf('Prev (I,Q): (%i,%i); Angle: %2f\n',PrevAdjIA, PrevAdjQA, rad2deg(PrevAngle));
-        fprintf('Curr (I,Q): (%i,%i); Angle: %2f\n',AdjIA, AdjQA, rad2deg(Angle));
-        fprintf('Diff: %2f, Cross: %i\n\n', rad2deg(Diff), Cross);
+        fprintf('Prev (I,Q) Ard: (%i,%i)\n',PrevIA, PrevQA);
+        fprintf('Prev (I,Q): (%i,%i); Angle: %2f\n',PrevIA, PrevQA, rad2deg(PrevAngle));
+        fprintf('Curr (I,Q): (%i,%i); Angle: %2f\n',CurrIA, CurrQA, rad2deg(CurrAngle));
+        fprintf('Diff: %2f, Cross: %i\n\n', rad2deg(DiffAngle), Cross);
         figure;
-        plot(PrevAdjQA, PrevAdjIA, '+', AdjQA, AdjIA, '*', 0, 0, 'o');
-        MaxX = max(abs(PrevAdjQM),abs(AdjQA)) + 1;
+        plot(PrevQA, PrevIA, '+', CurrQA, CurrIA, '*', 0, 0, 'o');
+        MaxX = max(abs(PrevAdjQM),abs(CurrQA)) + 1;
         xlim([-MaxX, MaxX]);
-        MaxY = max(abs(PrevAdjIM),abs(AdjIA)) + 1;
+        MaxY = max(abs(PrevAdjIM),abs(CurrIA)) + 1;
         ylim([-MaxY, MaxY]);
         
     end
     
     CurrCutsM = CurrCutsM + IsCutA;
     
-%     if SampleNo>=4251 && IsCutA == 1;
-%         fprintf('Sample No: %i, CurrCutsM: %i\n',SampleNo, CurrCutsM);
-%     end;
+    %     if SampleNo>=4251 && IsCutA == 1;
+    %         fprintf('Sample No: %i, CurrCutsM: %i\n',SampleNo, CurrCutsM);
+    %     end;
     
-%     if CurrCutsM ~= CurrCutsA
-%         fprintf('Curr Cuts Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, CurrCutsA, CurrCutsM);
-%         CurrCutsErrors = CurrCutsErrors + 1;
-%     end
-%     
-%     RunCutsM = RunCutsM + IsCutM;
-%     if RunCutsM ~= RunCutsA
-%         fprintf('Run Cuts Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, RunCutsA, RunCutsM);
-%         RunCutsErrors = RunCutsErrors + 1;
-%     end
+    %     if CurrCutsM ~= CurrCutsA
+    %         fprintf('Curr Cuts Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, CurrCutsA, CurrCutsM);
+    %         CurrCutsErrors = CurrCutsErrors + 1;
+    %     end
+    %
+    %     RunCutsM = RunCutsM + IsCutM;
+    %     if RunCutsM ~= RunCutsA
+    %         fprintf('Run Cuts Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, RunCutsA, RunCutsM);
+    %         RunCutsErrors = RunCutsErrors + 1;
+    %     end
     
     % -------------------------------------------------------------------
     %   Check for displacement & confirmation
@@ -207,16 +140,18 @@ for Idx = 1 : length(ArduinoInputsDetects)-1
         continue;
     end
     
-    % We are now on the last sample of the snippet. 
+    SnippetCount = SnippetCount + 1;
+    
+    % We are now on the last sample of the snippet.
     % Cuts have been validated and summed for the sample.
     % For the Arduino, displacement & confirmation are reported
     %   on the next sample. Hence Idx+1 in the index.
-    IsDispA = ArduinoInputsDetects{Idx + 1, DispC};
-    IsConfA = ArduinoInputsDetects{Idx + 1, ConfC};
-
+    IsDispA = ArduinoInputsDetects{Idx + 1, IsDispC};
+    IsConfA = ArduinoInputsDetects{Idx + 1, IsConfC};
+    
     
     IsDispM = (abs(CurrCutsM) >= MinCumCuts);
-
+    
     % Move the MofN values down
     for I = length(MofNBuff)-1 : -1 : 1
         MofNBuff(I + 1) = MofNBuff(I);
@@ -226,14 +161,14 @@ for Idx = 1 : length(ArduinoInputsDetects)-1
     % Check if sum of displacements is at least MofN_M
     NumDisp = sum(MofNBuff);
     IsConfM = (NumDisp >= MofN_M);
-
+    
     fprintf('\t- Sample No %i, CurrCutsM %i, IsDispA %i, IsDispM %i, IsConfA %i, IsConfM %i\n', SampleNo, CurrCutsM, IsDispA, IsDispM, IsConfA, IsConfM);
-%     if DispA && ConfA
-%     elseif DispA
-%         fprintf('\t- Disp %i\n', CurrCutsM);
-%     elseif ConfA
-%         fprintf('\t- Conf %i\n', ConfA);
-%     end
+    %     if DispA && ConfA
+    %     elseif DispA
+    %         fprintf('\t- Disp %i\n', CurrCutsM);
+    %     elseif ConfA
+    %         fprintf('\t- Conf %i\n', ConfA);
+    %     end
     
     
     if IsDispM ~= IsDispA
@@ -251,12 +186,10 @@ for Idx = 1 : length(ArduinoInputsDetects)-1
 end
 
 fprintf('\n');
-% fprintf('Sampling Errors: %i\n', SamplingErrors);
-% fprintf('Interpolation Errors: %i\n', InterpolationErrors);
-% fprintf('Mean Calc Errors: %i\n', MeanErrors);
+fprintf('Input Count: %i\n',InputCount);
+fprintf('Snippet Count: %i\n',SnippetCount);
+fprintf('Cross Product vs. Angle Diff Errors: %i\n',CrossAngleErrors);
 fprintf('Cut Errors: %i\n', CutErrors);
-%fprintf('Curr Cut Errors: %i\n', CurrCutsErrors);
-%fprintf('Run Cut Errors: %i\n', RunCutsErrors);
 fprintf('Disp Errors: %i\n', DispErrors);
 fprintf('Conf Errors: %i\n', ConfErrors);
 
--- Arduino/Apps/Bumblebee_Displacement_Detector/Validation/ValidateArduinoInputs.m
+++ Arduino/Apps/Bumblebee_Displacement_Detector/Validation/ValidateArduinoInputs.m
@@ -3,58 +3,58 @@ if ~exist('ArduinoValidationInputs','var')
     [~,~,ArduinoValidationInputs] = xlsread('Arduino Log - Validation Inputs.xlsx');
 end
 
-LineTypeC = 2;
-SampleNoC = 3;
-IValC = 4;
-QValC = 5;
-SumIC = 6;
-SumQC = 7;
-SampIC = 8;
-SampQC = 9;
-AdjIC = 10;
-AdjQC = 11;
-PrevAdjIC = 12;
-PrevAdjQC = 13;
+SnippetSize = 250;
 
-% SnippetSize = 250;
-% MinCumCuts = 6;
-% MofN_M = 2;
-% MofN_N = 8;
-% MofNBuff = zeros(1,MofN_N);
+[NRows, NCols] = size(ArduinoValidationInputs);
+TagCol = -1;
+for I = 1:NCols
+    if strcmp(ArduinoValidationInputs(1,I),'#c')
+        TagCol = I;
+        break;
+    end
+end
 
+if TagCol < 1
+    fprintf('Tag column not found\nTerminating\n');
+    return;
+end
 
-% IsCutC = 14;
-% CurrCutsC = 15;
-% RunCutsC = 16;
-% DispC = 17;
-% ConfC = 18;
+LineTypeC = TagCol + 0;
+SampleNoC = TagCol + 1;
+IValC = TagCol + 2;
+QValC = TagCol + 3;
+SumIC = TagCol + 4;
+SumQC = TagCol + 5;
+SampIC = TagCol + 6;
+SampQC = TagCol + 7;
+AdjIC = TagCol + 8;
+AdjQC = TagCol + 9;
+PrevAdjIC = TagCol + 10;
+PrevAdjQC = TagCol + 11;
 
 SumIValsM = 0;
 SumQValsM = 0;
 
+InputCount = 0;
 SamplingErrors = 0;
 InterpolationErrors = 0;
 MeanErrors = 0;
 CutErrors = 0;
-CurrCutsErrors = 0;
-RunCutsErrors = 0;
-DispErrors = 0;
-ConfErrors = 0;
 
-CurrCutsM = 0;
-RunCutsM = 0;
-
-for Idx = 1 : length(ArduinoValidationInputs)-1
-    if ~strcmp(ArduinoValidationInputs(Idx, LineTypeC), '#4') || isnan(ArduinoValidationInputs{Idx+1,1})
+for Idx = 1 : NRows - 1
+    NextCol1 = ArduinoValidationInputs{Idx+1,1};
+    if ~strcmp(ArduinoValidationInputs(Idx, LineTypeC), '#i') || isnan(NextCol1(1))
         continue;
     end
+    
+    InputCount = InputCount + 1;
     
     SampleNo = ArduinoValidationInputs{Idx, SampleNoC};
     IA = ArduinoValidationInputs{Idx, IValC};
     QA = ArduinoValidationInputs{Idx, QValC};
     
     if mod(SampleNo,SnippetSize) == 0
-        fprintf('* Snippet %i, Sample %i\n',mod(SampleNo,SnippetSize), SampleNo);
+        fprintf('* Snippet %i, Sample %i\n',SampleNo/SnippetSize, SampleNo);
     end
     
     SumIValsM = SumIValsM + IA;
@@ -73,15 +73,8 @@ for Idx = 1 : length(ArduinoValidationInputs)-1
     PrevAdjIA = ArduinoValidationInputs{Idx, PrevAdjIC};
     PrevAdjQA = ArduinoValidationInputs{Idx, PrevAdjQC};
     
-%     IsCutA = ArduinoValidationInputs{Idx, IsCutC};
-%     CurrCutsA = ArduinoValidationInputs{Idx, CurrCutsC};
-%     RunCutsA = ArduinoValidationInputs{Idx, RunCutsC};
-%     DispA = ArduinoValidationInputs{Idx, DispC};
-%     ConfA = ArduinoValidationInputs{Idx, ConfC};
-    
     PrevAdjIM = ArduinoValidationInputs{Idx-1, AdjIC};
     PrevAdjQM = ArduinoValidationInputs{Idx-1, AdjQC};
-    
     
     % Check integrity of the data
     
@@ -116,125 +109,11 @@ for Idx = 1 : length(ArduinoValidationInputs)-1
         fprintf('Mean Calculation Error. SampleNo %i\n', SampleNo);
         MeanErrors = MeanErrors + 1;
     end
-    
-%     % Check the cut analysis using previous and current values
-%     %   We've validated the mean-adjusted sample inputs so we'll just use
-%     %   them.
-%     
-%     PrevAngle = atan2(PrevAdjIM, PrevAdjQM);
-%     %     if PrevAngle < 0
-%     %         PrevAngle = PrevAngle + 2*pi;
-%     %     end
-%     Angle = atan2(AdjIA, AdjQA);
-%     %     if Angle < 0
-%     %         Angle = Angle + 2*pi;
-%     %     end
-%     %     Diff1 = mod(abs(Angle - PrevAngle), 2*pi);
-%     %     if Diff1 > pi
-%     %         Diff = -(2*pi - Diff1);
-%     %     else
-%     %         Diff = Diff1;
-%     %     end
-%     %Diff = Angle - PrevAngle;
-%     
-%     Diff = angleDiff(PrevAngle, Angle);
-%     if round(mod(Diff,pi),5) == 0
-%         Diff = 0;
-%     end
-%     
-%     IsCutM = 0;
-%     
-%     % Clockwise: See if going from negative to positive
-%     if Diff < 0 && PrevAdjIM < 0 && AdjIA > 0
-%         IsCutM = +1;
-%     end
-%     
-%     % Counter-clockwise: See if going from positive to negative
-%     if Diff > 0 && PrevAdjIM > 0 && AdjIA < 0
-%         IsCutM = -1;
-%     end
-%     
-%     if IsCutM ~= IsCutA
-%         fprintf('Cut Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, IsCutA, IsCutM);
-%         CutErrors = CutErrors + 1;
-%         
-%         Cross = (PrevAdjQM * AdjIA) - (PrevAdjIM * AdjQA);
-%         fprintf('SampNo: %i\n', SampleNo);
-%         fprintf('Prev (I,Q) Ard: (%i,%i)\n',PrevAdjIA, PrevAdjQA);
-%         fprintf('Prev (I,Q): (%i,%i); Angle: %2f\n',PrevAdjIM, PrevAdjQM, rad2deg(PrevAngle));
-%         fprintf('Curr (I,Q): (%i,%i); Angle: %2f\n',AdjIA, AdjQA, rad2deg(Angle));
-%         fprintf('Diff: %2f, Cross: %i\n\n', rad2deg(Diff), Cross);
-%         figure;
-%         plot(PrevAdjQM, PrevAdjIM, '+', AdjQA, AdjIA, '*', 0, 0, 'o');
-%         MaxX = max(abs(PrevAdjQM),abs(AdjQA)) + 1;
-%         xlim([-MaxX, MaxX]);
-%         MaxY = max(abs(PrevAdjIM),abs(AdjIA)) + 1;
-%         ylim([-MaxY, MaxY]);
-%         
-%     end
-%     
-%     CurrCutsM = CurrCutsM + IsCutM;
-%     if CurrCutsM ~= CurrCutsA
-%         fprintf('Curr Cuts Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, CurrCutsA, CurrCutsM);
-%         CurrCutsErrors = CurrCutsErrors + 1;
-%     end
-%     
-%     RunCutsM = RunCutsM + IsCutM;
-%     if RunCutsM ~= RunCutsA
-%         fprintf('Run Cuts Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, RunCutsA, RunCutsM);
-%         RunCutsErrors = RunCutsErrors + 1;
-%     end
-%     
-%     % -------------------------------------------------------------------
-%     %   Check for displacement & confirmation
-%     % -------------------------------------------------------------------
-%     
-%     if mod(SampleNo,SnippetSize) ~= 0
-%         continue;
-%     end
-%     
-%     if DispA && ConfA
-%         fprintf('\t- Disp %i, Conf %i\n', CurrCutsA, ConfA);
-%     elseif DispA
-%         fprintf('\t- Disp %i\n', CurrCutsA);
-%     elseif ConfA
-%         fprintf('\t- Conf %i\n', ConfA);
-%     end
-%     
-%     DispM = (abs(CurrCutsM) >= MinCumCuts);
-%     
-%     if DispM ~= DispA
-%         fprintf('Disp Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, DispA, DispM);
-%         DispErrors = DispErrors + 1;
-%     end
-%     
-%     
-%     % Move the values down
-%     for I = length(MofNBuff)-1 : -1 : 1
-%         MofNBuff(I + 1) = MofNBuff(I);
-%     end
-%     MofNBuff(1) = DispM;
-%     NumDisp = sum(MofNBuff);
-%     
-%     ConfM = (NumDisp >= MofN_M);
-%     
-%     if ConfM ~= ConfA
-%         fprintf('Conf Error. Sample No: %i, Arduino: %i; MatLab: %i\n', SampleNo, ConfA, ConfM);
-%         ConfErrors = ConfErrors + 1;
-%     end
-%     
-%     CurrCutsM = 0;
-    
 end
 
 fprintf('\n');
+fprintf('Input Count: %i\n',InputCount);
 fprintf('Sampling Errors: %i\n', SamplingErrors);
 fprintf('Interpolation Errors: %i\n', InterpolationErrors);
 fprintf('Mean Calc Errors: %i\n', MeanErrors);
-% fprintf('Cut Errors: %i\n', CutErrors);
-% fprintf('Curr Cut Errors: %i\n', CurrCutsErrors);
-% fprintf('Run Cut Errors: %i\n', RunCutsErrors);
-% fprintf('Disp Errors: %i\n', DispErrors);
-% fprintf('Conf Errors: %i\n', ConfErrors);
-
 fprintf('\n');
