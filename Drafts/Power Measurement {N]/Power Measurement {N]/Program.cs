bfd8de09e5cdf98a99f0cb4f38371c841f36a4ef
William Leal <william.leal@samraksh.info>
2015-10-13 14:37:03 -0400

    Sync Power Measurement

---
+++ Drafts/Power Measurement {N]/Power Measurement {N]/Program.cs
@@ -0,0 +1,233 @@
+﻿/*******************************************
+ * Power Measurement
+ *      Used in conjunction with a USB power monitor to measure power utilization in different modes
+ * Version
+ *	1.0	
+ *		Initial version
+*******************************************/
+
+using System;
+using System.Reflection;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Samraksh.AppNote.Utility;
+using Samraksh.eMote.DotNow;
+#if UseRadio
+using Samraksh.eMote.Net.Mac;
+using Samraksh.eMote.Net.Radio;
+#endif
+
+namespace Samraksh.AppNote.PowerMeasurement
+{
+	/// <summary>
+	/// The program
+	/// </summary>
+	public class Program
+	{
+
+		private static readonly EnhancedEmoteLcd Lcd = new EnhancedEmoteLcd();
+
+		// Toggle mode switch
+		private static readonly InterruptPort ToggleMode = new InterruptPort(Pins.GPIO_J12_PIN1, false, Port.ResistorMode.PullUp,
+			Port.InterruptMode.InterruptEdgeHigh);
+
+		// Power modes
+		private enum ModeStates { Sleep, PowerLow, PowerMedium, PowerHigh, RadioRx, RadioTxLo, RadioTxHi, };
+		private static ModeStates _currMode;
+
+
+		private static class ModeFields
+		{
+			public static readonly ManualResetEvent CpuBoundEnable = new ManualResetEvent(false);
+			public static readonly ManualResetEvent RadioTxEnable = new ManualResetEvent(false);
+#if UseRadio
+			public static readonly SimpleCsmaRadio CsmaRadio = new SimpleCsmaRadio(RadioName.RF231RADIO, 100, TxPowerValue.Power_0Point0dBm, null);
+#endif
+			public static void DisableAll()
+			{
+				CpuBoundEnable.Reset();
+				RadioTxEnable.Reset();
+#if UseRadio
+				CsmaRadio.SetRadioState(SimpleCsmaRadio.RadioStates.Off);
+#endif
+			}
+		}
+
+		private static class ActivityThreads
+		{
+			public static void StartAll()
+			{
+				CpuBoundThread.Start();
+				RadioTxThread.Start();
+			}
+
+			private static readonly Thread CpuBoundThread = new Thread(() =>
+			{
+				var lastPowerLevel = -1;
+				while (true)
+				{
+					ModeFields.CpuBoundEnable.WaitOne();
+					if ((int)PowerState.CurrentPowerLevel == lastPowerLevel)
+					{
+						continue;
+					}
+					lastPowerLevel = (int)PowerState.CurrentPowerLevel;
+					Lcd.Write("P " + lastPowerLevel);
+				}
+				// ReSharper disable once FunctionNeverReturns
+			});
+
+
+			private static readonly Thread RadioTxThread = new Thread(() =>
+			{
+				var msg = new byte[1];
+				while (true)
+				{
+					ModeFields.RadioTxEnable.WaitOne();
+#if UseRadio
+					ModeFields.CsmaRadio.Send(Addresses.BROADCAST, msg);
+#endif
+				}
+				// ReSharper disable once FunctionNeverReturns
+			});
+		}
+
+
+		/// <summary>
+		/// Program entry point
+		/// </summary>
+		public static void Main()
+		{
+			Debug.Print("\nData Collector Radar");
+
+			// Print the version and build info
+			Debug.Print(VersionInfo.VersionBuild(Assembly.GetExecutingAssembly()));
+			Debug.Print("");
+
+			ToggleMode.OnInterrupt += ChangeMode.CallBack;
+
+			ModeFields.DisableAll();
+			ActivityThreads.StartAll();
+			
+			SetSleepMode();
+
+			Thread.Sleep(Timeout.Infinite);
+
+		}
+
+		private static class ChangeMode
+		{
+			private static DateTime _lastEventTime = DateTime.MinValue;
+			private static readonly TimeSpan BounceTime = new TimeSpan(0, 0, 0, 0, 300);
+
+			public static void CallBack(uint port, uint state, DateTime currEventTime) {
+				var isBbounceInterval = currEventTime - _lastEventTime < BounceTime;
+				// Debounce the switch
+				if (isBbounceInterval)
+				{
+					return;
+				}
+				_lastEventTime = currEventTime;
+
+				//Lcd.Write(ToggleMode.Read() ? 1 : 0);
+				//Thread.Sleep(500);
+
+				//	Toggle the mode
+				switch (_currMode)
+				{
+					case ModeStates.Sleep:
+						SetPowerLowMode();
+						break;
+					case ModeStates.PowerLow:
+						SetPowerMediumMode();
+						break;
+					case ModeStates.PowerMedium:
+						SetPowerHighMode();
+						break;
+					case ModeStates.PowerHigh:
+						SetRadioOnMode();
+						break;
+					case ModeStates.RadioRx:
+						SetRadioTxLo();
+						break;
+					case ModeStates.RadioTxLo:
+						SetRadioTxHi();
+						break;
+					case ModeStates.RadioTxHi:
+						SetSleepMode();
+						break;
+					default:
+						Lcd.Write("err");
+						throw new ApplicationException("Unhandled mode " + _currMode);
+				}
+			}
+		}
+
+		private static void SetSleepMode()
+		{
+			ModeFields.DisableAll();
+			_currMode = ModeStates.Sleep;
+			Lcd.Write("P 0");
+		}
+
+		private static void SetPowerLowMode()
+		{
+			ModeFields.DisableAll();
+			PowerState.ChangePowerLevel(PowerLevel.Low);
+			_currMode = ModeStates.PowerLow;
+			ModeFields.CpuBoundEnable.Set();
+		}
+
+		private static void SetPowerMediumMode()
+		{
+			ModeFields.DisableAll();
+			PowerState.ChangePowerLevel(PowerLevel.Medium);
+			_currMode = ModeStates.PowerMedium;
+			ModeFields.CpuBoundEnable.Set();
+		}
+
+		private static void SetPowerHighMode()
+		{
+			ModeFields.DisableAll();
+			PowerState.ChangePowerLevel(PowerLevel.High);
+			ModeFields.CpuBoundEnable.Set();
+			_currMode = ModeStates.PowerHigh;
+		}
+
+		private static void SetRadioOnMode()
+		{
+			ModeFields.DisableAll();
+#if UseRadio
+			ModeFields.CsmaRadio.SetRadioState(SimpleCsmaRadio.RadioStates.On);
+#endif
+			_currMode = ModeStates.RadioRx;
+			Lcd.Write("Rdo");
+		}
+
+		private static void SetRadioTxLo()
+		{
+			ModeFields.DisableAll();
+#if UseRadio
+			ModeFields.CsmaRadio.MacConfig.radioConfig.SetTxPower(TxPowerValue.Power_Minus17dBm);
+			ModeFields.CsmaRadio.SetRadioState(SimpleCsmaRadio.RadioStates.On);
+#endif
+			ModeFields.RadioTxEnable.Set();
+			_currMode = ModeStates.RadioTxLo;
+			Lcd.Write("Tx 0");
+		}
+
+		private static void SetRadioTxHi()
+		{
+			ModeFields.DisableAll();
+#if UseRadio
+			ModeFields.CsmaRadio.MacConfig.radioConfig.SetTxPower(TxPowerValue.Power_3dBm);
+			ModeFields.CsmaRadio.SetRadioState(SimpleCsmaRadio.RadioStates.On);
+#endif
+			ModeFields.RadioTxEnable.Set();
+			_currMode = ModeStates.RadioTxHi;
+			Lcd.Write("Tx 1");
+		}
+
+	}
+}
---
+++ Drafts/Power Measurement {N]/Utility/SimpleCsmaRadio.cs
@@ -0,0 +1,165 @@
+#if UseRadio
+/********************************************************
+ * Simple CSMA Radio class
+ *  Simplify the definition of a CSMA radio
+ * Versions
+ *  1.0 Initial version
+ *  1.1 Added parameter for type of radio
+ *  1.2 Revised for eMote namespace
+ *  1.3 Minor changes and corrections
+ *  1.4
+ *		- Changed obselete method calls
+ *	1.5	Added property MacConfig
+ *********************************************************/
+
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+
+using Samraksh.eMote.Net;
+using Samraksh.eMote.Net.Mac;
+using Samraksh.eMote.Net.Radio;
+
+namespace Samraksh.AppNote.Utility {
+    /// <summary>
+    /// Handle CSMA radio communication
+    /// To keep it simple, we ignore neighborhood changes
+    /// </summary>
+    public class SimpleCsmaRadio {
+
+        /// <summary>
+        /// Callback delegate for user method to handle incoming packets
+        /// </summary>
+        /// <param name="csma"></param>
+        public delegate void RadioReceivedData(CSMA csma);
+
+        readonly RadioReceivedData _userReceivedDataCallback;
+
+        // CSMA object that's created & passed back to the user.
+        readonly CSMA _csma;
+
+		/// <summary>
+		/// Mac configuration, available to the user
+		/// </summary>
+		public MacConfiguration MacConfig { private set; get; }
+
+        /// <summary>
+        /// Radio states
+        /// </summary>
+        public enum RadioStates {
+            /// <summary>Radio on</summary>
+            On,
+
+            /// <summary>Radio off</summary>
+            Off
+        };
+
+	    /// <summary>
+	    /// CSMA radio constructor 
+	    /// </summary>
+	    /// <param name="radioName">Name of the radio (internal, long range)</param>
+	    /// <param name="ccaSensetime">CCA sense time, in ms</param>
+	    /// <param name="txPowerValue">Power level</param>
+	    /// <param name="radioReceivedData">Method to call when data received. Can be null if user does not want to be notified of received messages</param>
+	    /// <param name="channel">Channel to use</param>
+	    public SimpleCsmaRadio(RadioName radioName, byte ccaSensetime, TxPowerValue txPowerValue, RadioReceivedData radioReceivedData, Channels channel = Channels.Channel_26) {
+            MacConfig = new MacConfiguration { NeighborLivenessDelay = 100, CCASenseTime = ccaSensetime };
+            MacConfig.radioConfig.SetTxPower(txPowerValue);
+            MacConfig.radioConfig.SetRadioName(radioName);
+			MacConfig.radioConfig.SetChannel(channel);
+            _userReceivedDataCallback = radioReceivedData;
+
+            try {
+                var retVal = MACBase.Configure(MacConfig, Receive, NeighborChange);
+                    // Set up CSMA with the MAC configuration, receive callback and neighbor change callback (which does nothing)
+                if (retVal != DeviceStatus.Success) {
+                    var lcd = new EnhancedEmoteLcd();
+                    lcd.Write("5555");
+                    Thread.Sleep(Timeout.Infinite);
+                }
+
+                _csma = CSMA.Instance;
+            }
+            catch (MacNotConfiguredException e) {
+                Debug.Print("CSMA configuration error " + e);
+                var lcd = new EnhancedEmoteLcd();
+                lcd.Write("1111");
+                Thread.Sleep(Timeout.Infinite);
+                throw;
+            }
+            catch (Exception e) {
+                Debug.Print("Unknown error " + e);
+                var lcd = new EnhancedEmoteLcd();
+                lcd.Write("2222");
+                Thread.Sleep(Timeout.Infinite);
+                throw;
+            }
+            Debug.Print("CSMA address is :  " + _csma.GetAddress());
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <param name="msgType">Message type: broadcast or CSMA address of recipient</param>
+        /// <param name="message">Message to be sent, as a byte array</param>
+        public void Send(Addresses msgType, byte[] message) {
+            //var lcd = new EnhancedEmoteLcd();
+            //lcd.Display("3333");
+            _csma.Send((ushort)msgType, message, 0, (ushort)message.Length);
+            //Thread.Sleep(100);
+            //lcd.Display("4444");
+        }
+
+        /// <summary>
+        /// Set radio state
+        /// </summary>
+        /// <param name="radioState">Desired radio state</param>
+        /// <returns>Device status: Success, Fail, Ready, Busy</returns>
+        public DeviceStatus SetRadioState(RadioStates radioState) {
+            DeviceStatus resultStatus;
+            switch (radioState) {
+                case RadioStates.On: {
+                        resultStatus = _csma.GetRadio().TurnOn();
+                        break;
+                    }
+                case RadioStates.Off: {
+                        resultStatus = _csma.GetRadio().Sleep(0);
+                        break;
+                    }
+                default: {
+                        throw new Exception("Undefined RadioState: " + radioState);
+                    }
+            }
+            return resultStatus;
+        }
+
+
+
+        /// <summary>
+        /// Callback when neighborhood changes
+        /// </summary>
+        /// <remarks>
+        /// We are ignoring neighborhood changes so this method does nothing
+        /// </remarks>
+        /// <param name="numberOfNeighbors"></param>
+        public static void NeighborChange(UInt16 numberOfNeighbors) { }
+
+        /// <summary>
+        /// Callback when radio message received
+        /// </summary>
+        /// <remarks>
+        /// If user callback is not null then call with CSMA object
+        /// </remarks>
+        /// <param name="numberOfPackets"></param>
+        private void Receive(UInt16 numberOfPackets) {
+            // If the user doesn't want to be notified of received messages, return
+            if (_userReceivedDataCallback == null) {
+                return;
+            }
+            // Send the CSMA object to the user.
+            // No need to send numberOfPackets; that's available as CSMA.GetPendingPacketCount
+            _userReceivedDataCallback(_csma);
+        }
+    }
+}
+#endif
