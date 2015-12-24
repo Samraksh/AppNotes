4d057bc54a7304b0d7700f772301ec0708bdf046
William Leal <william.leal@samraksh.info>
2015-12-24 13:05:27 -0500

    SimpleCSMA

--- Active/Utility Classes/DotNow/SimpleCSMA/SimpleCsmaRadio/Properties/AssemblyInfo.cs
+++ Active/Utility Classes/DotNow/SimpleCSMA/SimpleCsmaRadio/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleCsmaRadio")]
+[assembly: AssemblyTitle("SimpleCSMA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleCsmaRadio")]
+[assembly: AssemblyProduct("SimpleCSMA")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Active/Utility Classes/DotNow/SimpleCSMA/SimpleCsmaRadio/SimpleCSMA.cs
+++ Active/Utility Classes/DotNow/SimpleCSMA/SimpleCsmaRadio/SimpleCSMA.cs
@@ -46,18 +46,30 @@ namespace Samraksh.AppNote.Utility
 	/// Handle CSMA radio communication
 	/// To keep it simple, we ignore neighborhood changes
 	/// </summary>
-	public class SimpleCsmaRadio
+	public class SimpleCSMA
 	{
 		/// <summary>
-		/// Callback delegate for user method to handle incoming packets
+		/// Callback delegate for OnReceive events
 		/// </summary>
 		/// <param name="csma"></param>
 		public delegate void ReceivePacket(CSMA csma);
 
 		/// <summary>
-		/// Receive event
+		/// ReceiveHandler event
 		/// </summary>
 		public event ReceivePacket OnReceive;
+
+		/// <summary>
+		/// Callback delegate for NeighborChange events
+		/// </summary>
+		/// <param name="numberOfNeighbors"></param>
+		/// <param name="csma"></param>
+		public delegate void NeighborChange(ushort numberOfNeighbors, CSMA csma);
+
+		/// <summary>
+		/// ReceiveHandler event
+		/// </summary>
+		public event NeighborChange OnNeighborChange;
 
 		// CSMA object that's created & passed back to the user.
 		private readonly CSMA _csma;
@@ -71,29 +83,60 @@ namespace Samraksh.AppNote.Utility
 			On,
 			/// <summary>Radio off</summary>
 			Off
-		};
+		}
+
+		/// <summary>CCA sense time</summary>
+		public byte CCASenseTime
+		{
+			get { return _ccaSenseTime; }
+			set { _ccaSenseTime = value; }
+		}
+		private byte _ccaSenseTime = 100;
+
+		/// <summary>Tx power value</summary>
+		public TxPowerValue TxPowerValue
+		{
+			get { return _txPowerValue; }
+			set { _txPowerValue = value; }
+		}
+		private TxPowerValue _txPowerValue = TxPowerValue.Power_3dBm;
+
+		/// <summary>Channel</summary>
+		public Channels Channel
+		{
+			get { return _channel; }
+			set { _channel = value; }
+		}
+		private Channels _channel = Channels.Channel_26;
+
+		/// <summary>Neighbor Liveness Delay</summary>
+		public uint NeighborLivenessDelay
+		{
+			get { return _neighborLivenessDelay; }
+			set { _neighborLivenessDelay = value; }
+		}
+		private uint _neighborLivenessDelay = 100;
+
+		private readonly MacConfiguration _macConfig;
 
 		/// <summary>
 		/// CSMA radio constructor 
 		/// </summary>
 		/// <param name="radioName">Name of the radio (internal, long range)</param>
-		/// <param name="ccaSensetime">CCA sense time, in ms</param>
-		/// <param name="txPowerValue">Power level</param>
-		/// <param name="channel">Channel to use</param>
-		public SimpleCsmaRadio(RadioName radioName, byte ccaSensetime, TxPowerValue txPowerValue, Channels channel = Channels.Channel_26)
+		public SimpleCSMA(RadioName radioName)
 		{
-			var macConfig = new MacConfiguration
+			_macConfig = new MacConfiguration
 			{
-				NeighborLivenessDelay = 100,
-				CCASenseTime = ccaSensetime
+				NeighborLivenessDelay = NeighborLivenessDelay,
+				CCASenseTime = CCASenseTime
 			};
-			macConfig.radioConfig.SetTxPower(txPowerValue);
-			macConfig.radioConfig.SetRadioName(radioName);
-			macConfig.radioConfig.SetChannel(channel);
+			_macConfig.radioConfig.SetTxPower(TxPowerValue);
+			_macConfig.radioConfig.SetRadioName(radioName);
+			_macConfig.radioConfig.SetChannel(Channel);
 
 			try
 			{
-				var retVal = MACBase.Configure(macConfig, Receive, NeighborChange);
+				var retVal = MACBase.Configure(_macConfig, ReceiveHandler, NeighborChangeHandler);
 				// Set up CSMA with the MAC configuration, receive callback and neighbor change callback (which does nothing)
 				if (retVal != DeviceStatus.Success)
 				{
@@ -119,10 +162,26 @@ namespace Samraksh.AppNote.Utility
 		/// <param name="radioName">Name of the radio (internal, long range)</param>
 		/// <param name="ccaSensetime">CCA sense time, in ms</param>
 		/// <param name="txPowerValue">Power level</param>
+		/// <param name="channel">Channel to use</param>
+		[Obsolete("Deprecated. Use SimpleCSMA(RadioName) with properties")]
+		public SimpleCSMA(RadioName radioName, byte ccaSensetime, TxPowerValue txPowerValue,
+			Channels channel = Channels.Channel_26) : this(radioName)
+		{
+			_macConfig.CCASenseTime = CCASenseTime = ccaSensetime;
+			_macConfig.radioConfig.SetTxPower(TxPowerValue = txPowerValue);
+			_macConfig.radioConfig.SetChannel(Channel = channel);
+		}
+
+		/// <summary>
+		/// CSMA radio constructor 
+		/// </summary>
+		/// <param name="radioName">Name of the radio (internal, long range)</param>
+		/// <param name="ccaSensetime">CCA sense time, in ms</param>
+		/// <param name="txPowerValue">Power level</param>
 		/// <param name="receivePacket">Method to call when data received. Can be null if user does not want to be notified of received messages</param>
 		/// <param name="channel">Channel to use</param>
 		[Obsolete("Use constructor without ReceivePacket argument with OnReceive += to subscribe")]
-		public SimpleCsmaRadio(RadioName radioName, byte ccaSensetime, TxPowerValue txPowerValue, ReceivePacket receivePacket, Channels channel = Channels.Channel_26) :
+		public SimpleCSMA(RadioName radioName, byte ccaSensetime, TxPowerValue txPowerValue, ReceivePacket receivePacket, Channels channel = Channels.Channel_26) :
 			this(radioName, ccaSensetime, txPowerValue, channel)
 		{
 			if (receivePacket != null)
@@ -176,7 +235,13 @@ namespace Samraksh.AppNote.Utility
 		/// We are ignoring neighborhood changes so this method does nothing
 		/// </remarks>
 		/// <param name="numberOfNeighbors"></param>
-		public static void NeighborChange(UInt16 numberOfNeighbors) { }
+		public void NeighborChangeHandler(ushort numberOfNeighbors)
+		{
+			if (OnNeighborChange != null)
+			{
+				OnNeighborChange(numberOfNeighbors, _csma);
+			}
+		}
 
 		/// <summary>
 		/// Callback when radio message received
@@ -185,7 +250,7 @@ namespace Samraksh.AppNote.Utility
 		/// If user callback is not null then call with CSMA object
 		/// </remarks>
 		/// <param name="numberOfPackets"></param>
-		private void Receive(UInt16 numberOfPackets)
+		private void ReceiveHandler(ushort numberOfPackets)
 		{
 			// Send the CSMA object to the user.
 			// No need to send numberOfPackets; that's available as CSMA.GetPendingPacketCount
