4b53701d038dcb7885464986258b123afeec4cda
William Leal <william.leal@samraksh.info>
2015-12-30 14:51:23 -0500

    Health Monitor Node-Controller

---
+++ Drafts/Health Monitor/Node-Controller/Node-Controller/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Node_Controller
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Debug.Print(Resources.GetString(Resources.StringResources.String1));
+		}
+	}
+}
