843e2a2ec8692192330635a7b58dd2a29130b811
William Leal <william.leal@samraksh.info>
2016-01-13 12:20:01 -0500

    More setup-related stuff + Doc X

---
+++ Active/App Notes/Wireless Data Collector [N]/Current/Wireless Data Collector - Sensing/Wireless Data Collector - Sensing/Samraksh DLLs/Samraksh_SPOT_RealTime/RealTimeTimer/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.SPOT.Hardware;
+
+namespace Samraksh.SPOT.RealTime
+{
+    public class Timer : NativeEventDispatcher
+    {
+        public Timer(string strDrvName, ulong drvData, int callbackCount) 
+            : base(strDrvName, drvData) 
+        {
+
+        }
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static public void Dispose();
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static public bool Change(uint dueTime, uint period);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static public void GenerateInterrupt(); 
+    }
+}
