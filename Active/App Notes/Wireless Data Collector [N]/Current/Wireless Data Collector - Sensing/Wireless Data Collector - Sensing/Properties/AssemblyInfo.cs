f4bb6e376730a37077d7fabd263b8c83fd754673
William Leal <william.leal@samraksh.info>
2017-07-11 18:12:02 -0400

    Getting Wireless Data Collector going for eMote 4.3.0.14

--- Active/App Notes/Wireless Data Collector [N]/Current/Wireless Data Collector - Sensing/Wireless Data Collector - Sensing/Properties/AssemblyInfo.cs
+++ Active/App Notes/Wireless Data Collector [N]/Current/Wireless Data Collector - Sensing/Wireless Data Collector - Sensing/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Wireless Data Collector - Sensing")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,4 +21,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
